689bb90eb91fb1691733ffb6df4d5259ecb2f4ba
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-15 05:00:31 +0430

    Add files via upload

---
+++ innerWidth-innerHeight.js
@@ -0,0 +1,6 @@
+$("button").click(function(){
+ var txt = "";
+ txt+= "innerwidth: " + $("#div1").innerwidth() + "</br>";
+ txt+= "innerheight: " + $("#div1").innerheight()
+ $("#div1").html(txt)
+})
