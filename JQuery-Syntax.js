e5997fd6f8a7c88bdf950f3277b6b732329c4e42
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-15 05:58:55 +0430

    Update JQuery-Syntax.js

--- JQuery-Syntax.js
+++ JQuery-Syntax.js
@@ -1,5 +1,6 @@
 /*
  Jquery Tutorials 
+ jquery Syntax 
  1 - A $ Sign to define/access jquery
  2 - A (selector) to "jquery (or find)" HTML elements
  3 - A JQuery action() to be performed on the elements
