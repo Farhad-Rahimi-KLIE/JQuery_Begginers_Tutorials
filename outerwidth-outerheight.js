11ba1b853b8c4f45aef9b0c149cf09b3cd1e5ad3
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-18 04:38:05 +0430

    Update outerwidth-outerheight.js

--- outerwidth-outerheight.js
+++ outerwidth-outerheight.js
@@ -2,7 +2,7 @@
 // jquery Tutorials
 $("button").click(function(){
  var txt = "";
- txt+= "outerwidth: " + $("#div3").Outerwidth() + "</br>";
- txt+= "outerheight: " + $("#div3").Outerheight()
+ txt+= "Outerwidth: " + $("#div3").Outerwidth() + "</br>";
+ txt+= "Outerheight: " + $("#div3").Outerheight()
  $("#div3").html(txt)
 })
